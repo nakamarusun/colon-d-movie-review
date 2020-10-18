32ef30c44acb4b4b8ca101d5156b4369ff7de35b
Jason Christian H <nakamarujc@gmail.com>
2020-10-18 16:11:21 +0700

    Added static file caching in the code, and tested css

---
+++ ColonD/__init__.py
@@ -0,0 +1,22 @@
+from flask import (Flask, render_template)
+import flask
+
+def create_app():
+
+    # Create the main app
+    app = Flask(__name__)
+
+    @app.route("/")
+    def index():
+        return render_template("home/index.html")
+
+    @app.route("/favicon.ico")
+    def img():
+        return "yes"
+
+    from ColonD.cache import reg_static_uncache
+    
+    # Registers the app so that the static will change cache properly.
+    reg_static_uncache(app)
+
+    return app
