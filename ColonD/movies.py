d31a9dc470fe9a7eaaf2ca07f07adbb461ed3941
Jason Christian H <nakamarujc@gmail.com>
2020-10-30 21:28:38 +0700

    Added not-working page switches, and debug failed html

--- ColonD/movies.py
+++ ColonD/movies.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, redirect, session
+from flask import Blueprint, request, redirect, session, current_app
 from flask.helpers import flash, url_for
 from flask.templating import render_template
 from ColonD import db
@@ -13,11 +13,23 @@ flavor_words = ["Great", "Fine", "Marvelous", "Pretty", "Cool"]
 
 @bp.route("/")
 def home():
+
+    content_per_page = int(request.args.get("amt", 10))
+    content_per_page = content_per_page if content_per_page < 20 else 20
+    page = int(request.args.get("pg", 1))
+
     cursor = db.mydb.cursor()
-    cursor.execute("SELECT movie_name, year_rel, runtime, director_name FROM movies m JOIN directors d ON m.director_id=d.id LIMIT 0, 10")
+    
+    # Gets all the movie details from an offset (display as page)
+    cursor.execute("SELECT movie_name, year_rel, runtime, director_name FROM movies m JOIN directors d ON m.director_id=d.id LIMIT {}, {}"
+    .format((page - 1) * content_per_page, content_per_page))
+
     query = cursor.fetchall()
 
-    small_image = False
+    # The amount of movie available in the site.
+    movies = len(query)
+
+    small_image = current_app.config['SMALL_IMAGE']
 
     # Code snippet to get all the urls for scraping google images.
 
@@ -31,7 +43,7 @@ def home():
         # If it is false, then the image got will be higher quality, and the user does
         # not have to load the link individually.
         if small_image:
-            html = get('https://www.google.com/search?q={}+movie+{}&tbm=isch'.format(row[0], row[3])).text
+            html = get('https://www.google.com/search?q={}+movie+poster+{}&tbm=isch'.format(row[0], row[3])).text
             bp = BeautifulSoup(html, 'html.parser')
             img = bp.find_all('img')
             
@@ -39,7 +51,7 @@ def home():
         else:
             # The header is there so that as if a windows computer is accessing the website.
             # This will provide a different response from small_image=True.
-            html = get('https://www.google.com/search?q={}+movie+{}&tbm=isch'.format(row[0], row[3]), headers={
+            html = get('https://www.google.com/search?q={}+movie+poster+{}&tbm=isch'.format(row[0], row[3]), headers={
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0", "Accept": "image/webp,*/*"
                 }).text
             # Defines a RegEx pattern in which the image is contained
@@ -50,6 +62,9 @@ def home():
 
             img = None
             if not raw_data:
+                # Debugging purposes
+                with open("failed_html.html", "wb+") as file:
+                    file.write(html)
                 # Placeholder image.
                 img = url_for('static', filename='title.png')
             else:
